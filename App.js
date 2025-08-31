5c5b7b481bf37de4f13bb9f000ed66b1406f00fa
calebmutisya <mutisyacalebmusau@gmail.com>
2025-08-31 20:48:30 +0300

    expo setup

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Caleb!</Text>
+      <StatusBar style="auto" />
+      <Text style={{ fontSize: 28, color: 'red', fontWeight: 'bold' }}>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
