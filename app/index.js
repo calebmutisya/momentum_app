7533b6a10f4a5b5a10ae5fcf9376ebe88e0cb7d6
calebmutisya <mutisyacalebmusau@gmail.com>
2025-09-01 16:47:41 +0300

    implemented file based routing

---
+++ app/index.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.text}>Home</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text: {
+        fontSize: 30,
+        color: 'red'
+    }
+})
